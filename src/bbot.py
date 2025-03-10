39604c2cc2f4d7bab1a54ad98a37486a88a70373
Achkan Salehi <ashsalehi4133@gmail.com>
2025-03-10 17:52:39 +0100

    added mass

--- src/bbot.py
+++ src/bbot.py
@@ -3,21 +3,33 @@ import mujoco.viewer
 import numpy as np
 import sys
 import pdb
+from termcolor import colored
 
 # Load the model
 model = mujoco.MjModel.from_xml_path(sys.argv[1])
 data = mujoco.MjData(model)
-model.opt.gravity = (0, 0, 0)
+#model.opt.gravity = (0, 0, 0)
+
+#print masses 
+total_mass=0
+for b_i in range(model.nbody):
+    body_name=model.body(b_i).name
+    cur_mass=model.body(body_name).mass.item()
+    print(f"{body_name}: {cur_mass}")
+    total_mass+=cur_mass
+print(colored(f"total_mass: {total_mass}","magenta",attrs=["bold"]))
 
 
-with mujoco.viewer.launch_passive(model, data) as viewer:
-    #viewer.cam.distance = 500.0
-    #viewer.cam.azimuth = 90
-    #viewer.cam.elevation = -45
-    #viewer.cam.lookat[:] = np.array([0.0, -0.25, 0.824])
-    viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_JOINT] = True  #freejoints are showed as boxes that mask the hinge ones, so comment the former in the xml file for debug
-    while viewer.is_running():
-        mujoco.mj_step(model, data)
-        viewer.sync()
-        #pdb.set_trace()
 
+if 1:
+    with mujoco.viewer.launch_passive(model, data) as viewer:
+        #viewer.cam.distance = 500.0
+        #viewer.cam.azimuth = 90
+        #viewer.cam.elevation = -45
+        #viewer.cam.lookat[:] = np.array([0.0, -0.25, 0.824])
+        viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_JOINT] = True  #freejoints are showed as boxes that mask the hinge ones, so comment the former in the xml file for debug
+        while viewer.is_running():
+            mujoco.mj_step(model, data)
+            viewer.sync()
+            #pdb.set_trace()
+    
