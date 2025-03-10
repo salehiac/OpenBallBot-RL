30c86cec9aeb0d057cccd3776f22c78d8354626a
Achkan Salehi <ashsalehi4133@gmail.com>
2025-03-10 16:45:12 +0100

    added correct hinge joint axis and offsets

--- src/bbot.py
+++ src/bbot.py
@@ -8,12 +8,14 @@ import pdb
 model = mujoco.MjModel.from_xml_path(sys.argv[1])
 data = mujoco.MjData(model)
 model.opt.gravity = (0, 0, 0)
-# Create a viewer
+
+
 with mujoco.viewer.launch_passive(model, data) as viewer:
     #viewer.cam.distance = 500.0
     #viewer.cam.azimuth = 90
     #viewer.cam.elevation = -45
     #viewer.cam.lookat[:] = np.array([0.0, -0.25, 0.824])
+    viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_JOINT] = True  # Show joint axes
     while viewer.is_running():
         mujoco.mj_step(model, data)
         viewer.sync()
