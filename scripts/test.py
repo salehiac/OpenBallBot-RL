662ba219fd4ff4950ab31cd9fccdbb797648a38c
Achkan Salehi <ashsalehi4133@gmail.com>
2025-05-06 15:34:21 +0200

    very minor stuff. Note that the previous config leads to decent-looking training up to 5e6 timesteps (not collapse, no drift), but it does seems that policy parameter magnitude tends to grow. Anyway, three things to do: add weight decay, pretrain the CNN, and see whether changing terrain params helps

--- scripts/test.py
+++ scripts/test.py
@@ -8,6 +8,7 @@ import argparse
 from stable_baselines3 import PPO, SAC
 from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.common.callbacks import BaseCallback, CallbackList, CheckpointCallback
+from termcolor import colored
 
 sys.path.append("..")
 from utils import make_ballbot_env
@@ -17,31 +18,35 @@ def main(args):
 
     env=make_ballbot_env(gui=True,test_only=True,goal_type=args.goal_type)()
 
-    if args.algo=="ppo":
-        model = PPO.load(args.path)
-    elif args.algo=="sac":
-        model=SAC.load(args.path)
-    else:
-        raise Exception("unknown algo")
+    with torch.no_grad():
+        if args.algo=="ppo":
+            model = PPO.load(args.path)
+        elif args.algo=="sac":
+            model=SAC.load(args.path)
+        else:
+            raise Exception("unknown algo")
 
-    for test_i in range(args.n_test):
-        obs, _ = env.reset()
-        done = False
-           
-        G_tau=0
-        gamma=0.99999
-        count=0
-        while not done:
-            action, _ = model.predict(obs, deterministic=True)  # Use deterministic policy for testing
-            print(action)
-            obs, reward, done, truncated, info = env.step(action)
+        p_sum=sum([np.abs(param.sum().item()) for param in model.policy.parameters() if param.requires_grad])
+        print(colored(f"sum_of_model_params=={p_sum}","yellow"))
 
-            G_tau+=gamma**count*reward
-            count+=1
-            print(f'step={count}')
+        for test_i in range(args.n_test):
+            obs, _ = env.reset()
+            done = False
+               
+            G_tau=0
+            gamma=0.99999
+            count=0
+            while not done:
+                action, _ = model.predict(obs, deterministic=True)  # Use deterministic policy for testing
+                print(action)
+                obs, reward, done, truncated, info = env.step(action)
+
+                G_tau+=gamma**count*reward
+                count+=1
+                print(f'step={count}')
 
 
-        print("G_tau==",G_tau) 
+            print("G_tau==",G_tau) 
 
     env.close()
     
